94d9afc828ca01e8d0a419da1dbb9d157b3cf67e
Balaban Ann <balaban.ann92@gmail.com>
2018-10-31 01:10:42 +0200

    feat: add android setup constructors feat: add android device factory methods refactor: methods name in test

--- src/test/java/AndroidSetup.java
+++ src/test/java/AndroidSetup.java
@@ -1,27 +1,55 @@
 import java.net.URL;
+
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class AndroidSetup {
-    public String automationName;
-    public String appiumVersion;
-    public String platformName;
-    public String platformVersion;
-    public String deviceName;
-    public String udid;
-    public String appPackage;
-    public String appActivity;
-    public String URL;
-    public AndroidDriver mobiledriver;
+    private String automationName;
+    private String appiumVersion;
+    private String platformName;
+    private String platformVersion;
+    private String deviceName;
+    private String udid;
+    private String appPackage;
+    private String appActivity;
+    private String url;
+
+    //Constructor
+    public AndroidSetup(String platformVersion,
+                        String deviceName, String udid,
+                        String appPackage, String appActivity) {
+        this.automationName = "Appium";
+        this.appiumVersion = "1.9.1";
+        this.platformName = "Android";
+        this.platformVersion = platformVersion;
+        this.deviceName = deviceName;
+        this.udid = udid;
+        this.appPackage = appPackage;
+        this.appActivity = appActivity;
+        this.url = "http://0.0.0.0:4723/wd/hub";
+    }
+
+    //Factory method that creates new instance of Android Setup related to LG Device
+    public static AndroidSetup getLgDevice() {
+        return new AndroidSetup(
+                "7.1.2",
+                "and",
+                "LGD724273e98ee",
+                "com.google.android.youtube",
+                "com.google.android.apps.youtube.app.WatchWhileActivity");
+    }
+
+    //Factory method that creates new instance of Android Driver related to passed setup parameter
+    public static AndroidDriver<?> getDriver(AndroidSetup setup) throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("automationName", setup.automationName);
+        capabilities.setCapability("appiumVersion", setup.appiumVersion);
+        capabilities.setCapability("platformName", setup.platformName);
+        capabilities.setCapability("platformVersion", setup.platformName);
+        capabilities.setCapability("deviceName", setup.deviceName);
+        capabilities.setCapability("udid", setup.udid);
+        capabilities.setCapability("appPackage", setup.appPackage);
+        capabilities.setCapability("appActivity", setup.appActivity);
+        return new AndroidDriver<>(new URL(setup.url), capabilities);
+    }
 }
-
-
-//    DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("automationName", "Appium");
-//                capabilities.setCapability("appiumVersion", "1.9.1");
-//                capabilities.setCapability("platformName", "Android");
-//                capabilities.setCapability("platformVersion", "7.1.2");
-//                capabilities.setCapability("deviceName", "and");
-//                capabilities.setCapability("udid", "LGD724273e98ee");
-//                capabilities.setCapability("appPackage", "com.google.android.youtube");
-//                capabilities.setCapability("appActivity", "com.google.android.apps.youtube.app.WatchWhileActivity");
-//                mobiledriver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);//Your appium server url
--- src/test/java/YouTubeSearch.java
+++ src/test/java/YouTubeSearch.java
@@ -1,30 +1,15 @@
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 
 public class YouTubeSearch {
   public static AndroidDriver<?> mobiledriver;
 
     @BeforeTest
-    public void setUpAppiumDriver() throws Exception{
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appiumVersion", "1.9.1");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "7.1.2");
-        capabilities.setCapability("deviceName", "and");
-        capabilities.setCapability("udid", "LGD724273e98ee");
-        capabilities.setCapability("appPackage", "com.google.android.youtube");
-        capabilities.setCapability("appActivity", "com.google.android.apps.youtube.app.WatchWhileActivity");
-        mobiledriver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);//Your appium server url
+    public void setUp() throws Exception{
+        mobiledriver = AndroidSetup.getDriver(AndroidSetup.getLgDevice());
     }
 
     @Test
@@ -33,7 +18,7 @@ public class YouTubeSearch {
     }
 
     @AfterTest
-    public void closeAppiumDriver() throws Exception{
+    public void tearDown() throws Exception{
         mobiledriver.quit();
     }
 }
